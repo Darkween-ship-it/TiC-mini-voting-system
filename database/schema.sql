e14bbda38a04e55eb372612563f41f2a45f4622f
Haouabarkallah <haouabarkallah@gmail.com>
2026-08-20 18:51:35 +0200

    create votes table

--- database/schema.sql
+++ database/schema.sql
@@ -16,4 +16,16 @@ CREATE TABLE voters (
     has_voted BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-)
+);
+
+--create votes table
+CREATE TABLE votes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    voter_id INT NOT NULL,
+    option_id INT NOT NULL,
+    voted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+    FOREIGN KEY (option_id) REFERENCES voting_options(id) ON DELETE CASCADE,
+    CONSTRAINT unique_voter_cast UNIQUE (voter_id), -- prevents duplicate votes per person
+    
+);
